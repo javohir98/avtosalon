f63b3a08a6316bff41ec497ec540b334e9d99a90
javohir98 <jerkinov072@gmail.com>
2022-09-14 14:40:55 +0500

    .

--- src/components/car-inside/CarInside.js
+++ src/components/car-inside/CarInside.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Pannellum } from "pannellum-react";
+import myImg from '../../assets/360-img.jpg'
 
 const CarInside = ({img}) => {
   return (
     <Pannellum
         width="100%"
         height="500px"
-        image={img}
+        image={img || myImg}
         pitch={10}
         yaw={180}
         hfov={110}
